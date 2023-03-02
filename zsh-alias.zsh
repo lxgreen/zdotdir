302727dcd9a94ded747a93cbc6ec1bf8767fb59f
lxgreen <alxgreen@gmail.com>
2023-03-02 20:40:51 +0200

    aliases: file system commands

--- zsh-alias.zsh
+++ zsh-alias.zsh
@@ -5,7 +5,14 @@ alias zshrc="nvim $ZDOTDIR"
 #files
 alias rmf="rm -rf"
 alias dd='dirs -v'
-for index ({0..9}) alias "$index"="cd +${index}"; unset index
+for index ({1..9}) alias "$index"="cd +${index}"; unset index
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias take='function _take() { mkdir -p $1 && cd $1 }; _take $1'
 
 # npm
 alias regpub="npm config set registry https://registry.npmjs.org"
@@ -29,6 +36,7 @@ alias yb="yarn build"
 alias yyb="yarn && yarn build"
 alias yaw="yarn add -W"
 alias yt="yarn test"
+alias yyt="yarn && yarn test"
 
 #apps
 alias firefox='function _fox(){ (cd /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS && ./firefox $1) }; _fox $1'
@@ -38,13 +46,6 @@ alias fh='firefox_hardened NUL &'
 alias nvim='NVIM_LISTEN_ADDRESS=/tmp/nvimsocket nvim'
 alias nvr='NVIM_LISTEN_ADDRESS=/tmp/nvimsocket nvr'
 alias nn='nvim -u ~/.config/nvim/init.vim -S `pwd`/Session.vim'
-
-alias tt='tmux new -A -s rich-content'
-alias tn='tmux new -A -s neovim-ricos'
-alias ta='tmux a -t rich-content'
-alias zz='tmux new -A -s zettelkasten'
-alias tls='tmux list-sessions'
-
 alias pw='keepassxc-cli show ~/IDrive-Sync/Documents/keepass/pw.kdbx'
 alias man='batman'
 alias cat='bat'
@@ -55,23 +56,18 @@ alias bl="brew list"
 
 alias ww='w3m -no-mouse'
 alias -g CL="| xargs wc -l"
+alias -g G="| grep"
 alias ll='exa -lah --git'
 if [ -n "$(alias fd)" ]; then
   unalias fd
 fi
 
+#tmux
+alias zz='tmuxp load -y zettel'
+alias olo='tmuxp load -y olo'
+alias tls='tmux list-sessions'
+
 #git
-# alias g='git'
-# alias gup='git pull --rebase'
-# alias gco='git checkout'
-# alias gcm='git checkout master'
-# alias gcb='git checkout -b'
-# alias gm='git merge'
-# alias gcmsg='git commit -m'
-# alias gst='git status'
-# alias gd='git diff'
-# alias gf='git fetch'
-# alias gcl='git clone'
 alias gmm='gcm && gup && gco - && gm master'
 alias gcmrg='function _merge(){ git commit -m "Merge branch master into $1" };_merge `git branch --show-current`'
 alias gcmrgn='function _merge(){ git commit -m "Merge branch master into $1" -n };_merge `git branch --show-current`'
